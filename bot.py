661f749f7b40108c37eceab76bf204e764f668cf
mayankolhan <71210154+mayankolhan@users.noreply.github.com>
2022-08-21 09:37:01 +0530

    added comments
    added talk command 
    
    cleared some errors 
    
    unban command should work

--- bot.py
+++ bot.py
@@ -8,13 +8,10 @@ import random
 import requests
 import datetime
 
-
-from discord.ext import commands,tasks
+from discord.ext import commands, tasks
 from itertools import cycle
 
-status = cycle(["killing myself" , "not killing myself"])
-
-
+status = cycle(["killing myself", "not killing myself"])
 
 poke_lis = pd.read_csv("pokemon.csv")
 poke_arr = np.array(poke_lis["Name"].values)
@@ -31,22 +28,26 @@ token = dotenv_values("token.env")
 token = dict(token)
 guild_name = token["GUILD_TOKEN"]
 
+# guild is essentially server name GUILD_TOKEN is server name
+
 intent = discord.Intents.all()
-
 intent.members = True
-
-client = discord.Client(intents=intent)
+"""inent is required to access some features such as accessing names of people in the server"""
+# client = discord.Client(intents=intent)
 
 boti = commands.Bot(command_prefix="_", intents=intent)
+
+# boti will not be initialized if intents parameter is not passed
+# instead of discord.Intents.all() we can also use discord.Intents.default()
+# then use intent.members = True to get member names
 
 
 @boti.event
 async def on_ready():
-   # guild = discord.utils.get(client.guilds, name=guild_name)
-    #await boti.change_presence(status=discord.Status.online , activity=discord.Game("killing myself"))
+    # guild = discord.utils.get(client.guilds, name=guild_name)
+    # await boti.change_presence(status=discord.Status.online , activity=discord.Game("killing myself"))
     change_status.start()
     print("bot is ready")
-
 
     """print(f"{client.user} has connected to discord!" + f"\n {guild.name} (id: {guild.id})")"""
 
@@ -59,26 +60,29 @@ async def on_ready():
 #  for mem in guild.members:
 #      print(f"- {mem.name}")
 #      cnt+=1
+
+
 @tasks.loop(seconds=10)
 async def change_status():
     await boti.change_presence(activity=discord.Game(next(status)))
 
+
 @boti.event
 async def on_member_join(member):
     await member.create_dm()
-    await member.send(f"{member.name} WELCOME")
+    await member.send(f"{member.name} WELCOME to  {member.guild}")
 
 
-# @boti.event
-# async def on_message(message):
-#
-#     if("joke" in message.content):
-#         await message.channel.send("chup kar chutad")
+@boti.command()
+async def talk(ctx , *,message):
+
+    if("joke" in message):
+        await ctx.channel.send("chup kar chutad")
 
 
 @boti.command(name="pokemon", help="returns name of random pokemon")
 async def on_message(ctx):
-    if ctx.author == client.user:
+    if ctx.author == boti.user:
         return
 
     response = random.choice(poke_arr)
@@ -99,45 +103,43 @@ async def bad(ctx):
 
 
 @boti.command(help="enter word or sentence and will get a gif")
-async def gif(ctx, *,search):
+async def gif(ctx, *, search):
     api_key = "AIzaSyBE3EIUMTY6dgYF5_q8kpl7WSxW42g2484"
     client_key = "mk223"
     lim = 8
     r = requests.get("https://tenor.googleapis.com/v2/search?q=%s&key=%s&client_key=%s&limit=%s" % (
-    search, api_key, client_key, lim))
+        search, api_key, client_key, lim))
     if r.status_code == 200:
 
         top8 = json.loads(r.content)
 
-        await ctx.send(top8["results"][random.randint(0,7)]["url"])
+        await ctx.send(top8["results"][random.randint(0, 7)]["url"])
 
     else:
         await ctx.send("found nothing")
 
 
-
-
-@boti.command(help= "returns latency from bot to user which entered the command")
+@boti.command(help="returns latency from bot to user which entered the command")
 async def ping(ctx):
     await ctx.send(f"{round(boti.latency * 1000)}ms")
 
 
 @boti.command(help="enter number of text messages you want to delete in sever channel")
-@commands.has_permissions(administrator=True)
-async def clear(ctx,amount : int):
+@commands.has_permissions(manage_messages=True)
+async def clear(ctx, amount: int):
     await ctx.channel.purge(limit=amount)
 
 
 @boti.command(help="can be used to kick a member")
 @commands.has_permissions(administrator=True)
-async def kick(ctx , member :discord.Member, *, why=None):
+async def kick(ctx, member: discord.Member, *, why=None):
     await member.kick(reason=why)
     await ctx.send(f"kicked {member.mention}")
 
 
 @boti.command(help="cna be used to ban a member")
 @commands.has_permissions(administrator=True)
-async def ban(ctx , member : discord.Member , *,why=None):
+async def ban(ctx, member: discord.Member, *, why=None):
     await member.ban(reason=why)
     await ctx.send(f"banned {member.mention}")
 
@@ -149,7 +151,7 @@ async def unban(ctx, *, member):
         id_ = await commands.IDConverter().convert(str(member))
         if id_ is not None:
             try:
-                obj = await client.fetch_user(int(id_.group(1)))
+                obj = await boti.fetch_user(int(id_.group(1)))
             except discord.NotFound:
                 obj = None
         if obj is None:
@@ -159,39 +161,27 @@ async def unban(ctx, *, member):
     await ctx.send(f'Unbanned {obj}')
 
 
-      
-      
-@boti.command()
-async def spam(ctx, member : discord.Member):
-
-    await ctx.send(f"HELLO {member.mention} \n")
-
-    await ctx.send(f"HeLLO {member.mention}\n")
-
-    await ctx.send(f"HELLO {member.mention} \n")
-
-    await ctx.send(f"HeLLO {member.mention}\n")
-
-    await ctx.send(f"HELLO {member.mention} \n")
-
-    await ctx.send(f"HeLLO {member.mention}\n")
-
-
-
-
-
 @boti.command(help='displays hour , minutes , seconds and microseconds  respectively in that order')
 async def time(ctx):
     await ctx.send("TIMEZONE : IST \n")
     tm = str(datetime.datetime.time(datetime.datetime.now()))
 
-    await ctx.send(tm[:2]+" hours " + tm[3:5] + " minutes " + tm[6:10] + " seconds ")
+    await ctx.send(tm[:2] + " hours " + tm[3:5] + " minutes " + tm[6:10] + " seconds ")
 
 
+@boti.command()
+async def spam(ctx, member: discord.Member):
+    await ctx.send(f"HELLO {member.mention} \n")
 
+    await ctx.send(f"HeLLO {member.mention}\n")
+
+    await ctx.send(f"HELLO {member.mention} \n")
+
+    await ctx.send(f"HeLLO {member.mention}\n")
+
+    await ctx.send(f"HELLO {member.mention} \n")
+
+    await ctx.send(f"HeLLO {member.mention}\n")
 
 
 boti.run(token["DISCORD_TOKEN"])
-
-
-
